52a3bd0c6249d0dab5cb9f0070184f588bd6391e
Roman Shitov <romanshitov@yandex.ru>
2023-10-20 12:01:35 +0300

    change Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <nav className="cyan darken-3">
       <div className="nav-wrapper">
-        <a href="/" className="brand-logo">
+        <a href="/react-shop" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
