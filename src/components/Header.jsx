5ea3a354ed514bcac6ed0a76e34b090dcfdad29e
Roman Shitov <romanshitov@yandex.ru>
2023-10-20 11:35:39 +0300

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ export default function Header() {
   return (
     <nav className="cyan darken-3">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Sass</a>
+            <a
+              href="https://github.com/RomanShitovGH/react-shop"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Sass
+            </a>
           </li>
         </ul>
       </div>
